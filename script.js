cd5957d93e1b2a8bb446055be32ab1aeef09cdeb
mzeleny83 <110114227+mzeleny83@users.noreply.github.com>
2024-06-09 16:13:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".btn");
+button.addEventListener("click", () => {
+  //button.classList.remove("btn");
+  //button.classList.add("btn-ok");
+  button.classList.toggle("btn-ok");
+});
